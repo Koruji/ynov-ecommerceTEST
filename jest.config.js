b5ba02bf54d9539331d89bbedf15b21e6e2886bd
Koruji <jadecottin212@gmail.com>
2026-05-04 16:15:18 +0200

    chore: update coverage thresholds in Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,10 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.js', '!src/test/**/*.js'],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
     },
   },
   verbose: true,
